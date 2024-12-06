deb0d6ecde84cfc7b1a9505d43a24e75e988f48b
sbkr74 <sbkr74@gmail.com>
2024-12-06 20:42:13 +0530

    Major change1.0(logic changed to handle multiple QA

--- Quiz/Quiz_game/src/main_file.py
+++ Quiz/Quiz_game/src/main_file.py
@@ -7,20 +7,35 @@ with open(file_path,"r") as file:
     
 ques_dict = {}
 options_dict = {}
-
+ques_list = []
 for line in content:
-    if ":" in line:
+    if ":" in line and not line.startswith("Answer"):
+        if ques_dict:
+            ques_list.append(ques_dict)
+            ques_dict= {}
+            options_dict = {}
         key,value = line.split(":",1)
         key = key.strip()
         value = value.strip()
-        ques_dict[key]=value
+        ques_dict["Question"]=value  
+
+    elif line.startswith("Answer"):
+        _,answer = line.split(":",1)
+        answer = answer.strip()
+        ques_dict["Answer"] = answer
+
     elif "." in line:
         opt,val = line.split(".",1)
         opt = opt.strip()
         val = val.strip()
         options_dict[opt]=val
         ques_dict['Options']=options_dict
-ques_list = [ques_dict]
+    
+    
+    
+if ques_dict:
+    ques_list.append(ques_dict)
+
 score = 0
 for quest in ques_list:
     print(quest["Question"])
@@ -28,8 +43,8 @@ for quest in ques_list:
         print(opt,val)
     choice = input("Enter Your Choice: (A,B,C or D):-> ").upper()
     if choice == quest["Answer"][0]:
-        print("Hooray! Correct Answer...")
+        print("Hooray! Correct Answer...\n")
         score+=1
     else:
-        print("Looser!!! Wrong Answer. Correct answer: ",quest["Answer"])
+        print("Looser!!! Wrong Answer. Correct answer: ",quest["Answer"],"\n")
     print("Final Score:",score)
