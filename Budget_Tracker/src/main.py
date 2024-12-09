f9b8173052f0f7a40d44f03bdc759d8621ad1dcd
sbkr74 <sbkr74@gmail.com>
2024-12-09 19:52:13 +0530

    changes done for duplicate key items

--- Budget_Tracker/src/main.py
+++ Budget_Tracker/src/main.py
@@ -1,23 +1,26 @@
 def main():
     print("Welcome to Budget Tracker")
     initial_budget = float(input("Enter Your Budget for Expense Tracking: "))
-    
-    while True:
+    product_dict = {}
+    i=1
+    while initial_budget>0:
         print("What you like do?")
         print("1. Add expenses")
         print("2. Show budget details")
         print("3. Exit")
-
+        # print(product_dict)
         choice = input("Enter your choice (1/2/3) : ")
         if choice == "1":
             product = input("Enter name of product: ")
+            quantity = input("Enter in no. of item: ")
             price = float(input("Enter cost of the product: "))
-
+            product_dict[f"{i}. "+product]=f"{quantity}-₹{price}"
+            i+=1
             initial_budget -= price
 
         elif choice == "2":
-            print("Product:",product,"\t Cost:",price)
-            print("Remaining Balance: ",initial_budget)
+            for key,val in product_dict.items():
+                print("Product:",key,"Price:",val)
         
         elif choice == '3':
             return False
