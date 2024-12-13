caab766a80bc0ff44257f6b14af30fb32351b8f1
sbkr74 <sbkr74@gmail.com>
2024-12-13 22:27:24 +0530

    update1.1(minor changes)

--- Budget_Tracker/src/main_up1.py
+++ Budget_Tracker/src/main_up1.py
@@ -1,6 +1,7 @@
 def main():
     print("Welcome to Budget tracker App")
     budget = None
+    add_expense = None
     try:
         budget = float(input("Enter your proposed budget: "))
     except:
@@ -11,21 +12,22 @@ def main():
         print("2. View budget Details")
         print("3. Exit")
         ch = None
-        add_expense = None
+
         try:
             ch = int(input("Choose the options (1/2/3): "))
         except Exception as e:
-            print(e)
+            # print(e)                              # Debugging
             print("Error: Only Accepts Integer Value.")
 
         if ch == 1:
-            add_expense = "Expenses"  # replace it with function
+            # add_expense = "Expenses"              # replace it with function
+            add_expense = int(input("Enter Amount: "))
         elif ch == 2:
             if add_expense is None:
-                print("Show budget and all balance!")
+                print(f"Show budget and all balance!: {budget}")
                 print("expenses are not added")
             else:
-                view_budget = f"Show Budget {add_expense} and remaining balances"   # replace it with function
+                view_budget = f"Show Budget {add_expense} and remaining balances: {budget-add_expense}"   # replace it with function
                 print(view_budget)
         elif ch == 3:
             print("Exiting from App...")
