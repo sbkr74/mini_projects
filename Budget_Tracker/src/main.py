c1776fbcfaede3537a5b93e013f93cbd26956a22
sbkr74 <sbkr74@gmail.com>
2024-12-15 20:42:55 +0530

    update2.0[major changes(show budget part)]

--- Budget_Tracker/src/main.py
+++ Budget_Tracker/src/main.py
@@ -4,25 +4,32 @@ def main():
     product_dict = {}
     i=1
     while initial_budget>0:
-        print("What you like do?")
+        print("\nWhat you like do?")
         print("1. Add expenses")
         print("2. Show budget details")
         print("3. Exit")
-        # print(product_dict)
         choice = input("Enter your choice (1/2/3) : ")
         if choice == "1":
+            print("-"*25)
             product = input("Enter name of product: ")
             quantity = input("Enter in no. of item: ")
             price = float(input("Enter cost of the product: "))
-            product_dict[f"{i}. "+product]=f"{quantity}-₹{price}"
+            product_dict[f"{i}. "+product]=f"{quantity}->₹{price}"
             i+=1
             initial_budget -= price
 
         elif choice == "2":
+            total =0
+            sign = "₹"
+            print("-"*25)
             for key,val in product_dict.items():
                 print("Product:",key,"Price:",val)
+                pos = val.find(sign)
+                total += float(val[pos+1:])
+            print(f"\nTotal Expenditure:{sign}{total} \t Remaining Balance:{sign}{initial_budget}")
         
         elif choice == '3':
+            print("Goodbye! Signing Off Budget Tracker...\n")
             return False
     
 if __name__ == "__main__":
