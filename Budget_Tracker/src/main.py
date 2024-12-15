4f6a52e9754ed590fd1ed85eb4af92667898af64
sbkr74 <sbkr74@gmail.com>
2024-12-15 22:00:35 +0530

    update3.0[add_expense() defined]

--- Budget_Tracker/src/main.py
+++ Budget_Tracker/src/main.py
@@ -1,8 +1,12 @@
+def add_expense(expenses,description,amount):
+    expenses.append({"description":description,"amount":amount})
+    print(f"Added expense:{description}, Amount:{amount}")
+
 def main():
     print("Welcome to Budget Tracker")
     initial_budget = float(input("Enter Your Budget for Expense Tracking: "))
-    product_dict = {}
-    i=1
+    budget = initial_budget
+    expenses = []
     while initial_budget>0:
         print("\nWhat you like do?")
         print("1. Add expenses")
@@ -11,22 +15,18 @@ def main():
         choice = input("Enter your choice (1/2/3) : ")
         if choice == "1":
             print("-"*25)
-            product = input("Enter name of product: ")
-            quantity = input("Enter in no. of item: ")
-            price = float(input("Enter cost of the product: "))
-            product_dict[f"{i}. "+product]=f"{quantity}->₹{price}"
-            i+=1
-            initial_budget -= price
+            description = input("Enter name of product: ")
+            amount = float(input("Enter cost of the product: "))
+            add_expense(expenses,description,amount)
 
         elif choice == "2":
-            total =0
+            total = 0
             sign = "₹"
             print("-"*25)
-            for key,val in product_dict.items():
-                print("Product:",key,"Price:",val)
-                pos = val.find(sign)
-                total += float(val[pos+1:])
-            print(f"\nTotal Expenditure:{sign}{total} \t Remaining Balance:{sign}{initial_budget}")
+            for expense in expenses:
+                total+= expense['amount']                    
+                print(expense['description'],expense['amount'])
+            print(f"\nTotal Expenditure:{sign}{total} \t Remaining Balance:{sign}{budget-total}")
         
         elif choice == '3':
             print("Goodbye! Signing Off Budget Tracker...\n")
