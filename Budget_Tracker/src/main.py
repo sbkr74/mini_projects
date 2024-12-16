a8d125a369ab705b5fd0dbb001545a2b0935508c
sbkr74 <sbkr74@gmail.com>
2024-12-16 11:41:02 +0530

    update4.0[expense_calculation() defined]

--- Budget_Tracker/src/main.py
+++ Budget_Tracker/src/main.py
@@ -2,11 +2,19 @@ def add_expense(expenses,description,amount):
     expenses.append({"description":description,"amount":amount})
     print(f"Added expense:{description}, Amount:{amount}")
 
+def expense_calculation(expenses):
+    total = 0
+    for item in expenses:
+        total += item['amount']
+    return total
+    
+    
 def main():
     print("Welcome to Budget Tracker")
     initial_budget = float(input("Enter Your Budget for Expense Tracking: "))
     budget = initial_budget
     expenses = []
+    cal = 0 
     while initial_budget>0:
         print("\nWhat you like do?")
         print("1. Add expenses")
@@ -18,15 +26,14 @@ def main():
             description = input("Enter name of product: ")
             amount = float(input("Enter cost of the product: "))
             add_expense(expenses,description,amount)
+            cal = expense_calculation(expenses)
 
         elif choice == "2":
-            total = 0
-            sign = "₹"
             print("-"*25)
-            for expense in expenses:
-                total+= expense['amount']                    
+            print("Expenditure Details of Budget.")
+            for expense in expenses:                  
                 print(expense['description'],expense['amount'])
-            print(f"\nTotal Expenditure:{sign}{total} \t Remaining Balance:{sign}{budget-total}")
+            print(f"\nTotal Expenditure: {cal} \t Remaining Balance: {budget-cal}")
         
         elif choice == '3':
             print("Goodbye! Signing Off Budget Tracker...\n")
