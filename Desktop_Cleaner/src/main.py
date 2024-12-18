23a1aaf88b5b693ec9a6bc9fc5c7774e9543b3c3
sbkr74 <sbkr74@gmail.com>
2024-12-18 21:42:54 +0530

    update2.0[arreanged files]

--- Desktop_Cleaner/src/main.py
+++ Desktop_Cleaner/src/main.py
@@ -6,6 +6,7 @@ def create_subfolder(folder_path,sub_folder):
     sub_folder_path = os.path.join(folder_path,sub_folder)
     os.makedirs(sub_folder_path,exist_ok=True)
     return sub_folder_path
+
 def arrange_folder(folder_path):
     for filename in os.listdir(folder_path):
         if os.path.isfile(os.path.join(folder_path,filename)):
@@ -13,9 +14,14 @@ def arrange_folder(folder_path):
             if file_ext:
                 sub_folder = file_ext.upper()
                 sub_folder_path = os.path.join(folder_path,sub_folder)
+                file_path = os.path.join(folder_path,filename)
+                new_loc = os.path.join(sub_folder_path,file_path)
                 if not os.path.isdir(sub_folder_path):
                     create_subfolder(folder_path,sub_folder)
-                
+                    print(sub_folder_path)
+                if os.path.exists(new_loc):
+                    shutil.move(file_path,sub_folder_path)
+                    print('moved')
 
 if __name__ == '__main__':
     folder_path = r"D:\downloads"
