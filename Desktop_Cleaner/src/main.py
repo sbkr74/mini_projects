d53a223a64cec8263de77000a9c63ba62e598f01
sbkr74 <sbkr74@gmail.com>
2024-12-18 20:48:54 +0530

    update1.0(folders created)

--- Desktop_Cleaner/src/main.py
+++ Desktop_Cleaner/src/main.py
@@ -2,18 +2,20 @@ import os
 import shutil
 from pathlib import Path
 
+def create_subfolder(folder_path,sub_folder):
+    sub_folder_path = os.path.join(folder_path,sub_folder)
+    os.makedirs(sub_folder_path,exist_ok=True)
+    return sub_folder_path
 def arrange_folder(folder_path):
     for filename in os.listdir(folder_path):
         if os.path.isfile(os.path.join(folder_path,filename)):
-            # file_path = Path(filename)
-            # file_ext = file_path.suffix
             file_ext = filename.split('.')[-1].lower()
             if file_ext:
-                sub_folder = f"{file_ext.upper()}"
-                if not os.path.isdir(sub_folder):
-                    print("folder not created")
-                else:
-                    print("Already")
+                sub_folder = file_ext.upper()
+                sub_folder_path = os.path.join(folder_path,sub_folder)
+                if not os.path.isdir(sub_folder_path):
+                    create_subfolder(folder_path,sub_folder)
+                
 
 if __name__ == '__main__':
     folder_path = r"D:\downloads"
