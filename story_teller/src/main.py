c8cae57a4176ae3cf2bb61296213142be517681c
sbkr74 <sbkr74@gmail.com>
2025-01-08 20:46:06 +0530

    update1.0 (organised)

--- story_teller/src/main.py
+++ story_teller/src/main.py
@@ -1,4 +1,6 @@
 import random
+import json
+
 with open(r"story_teller\files\story.txt","r") as f:
     story = f.read()
 
@@ -15,28 +17,11 @@ for i,char in enumerate(story):
         words.add(word)
         start_word = -1
 
-answers = {"adjective": ("daring", "sunny", "foggy", "thrilling", "unforgettable"),
-        "terrain": ("forest", "canyon", "mountain"),
-        "weather condition": ("drizzle", "gusty winds", "clear skies", "rainy"),
-        "adventurous activity": ("trekking", "rafting", "ziplining", "skydiving","paragliding", "bungee jumping", "mountain climbing"),
-        "gear": ("rope", "helmet", "raft","Ice Axe"),
-        "noun": ("waterfall", "cliff","valley"),
-        "plural noun": ("backpack","gears","suits","attachemnts"),
-        "name of person 1": ("Shubham","Sumit","Aman"),
-        "name of person 2": ("Biruly","Himanshu","Prince"),
-        "name of person 3": ("Bittu","Suman","chunnu","krishna"),
-        "emotion": ("excited", "nervous", "accomplished","excited","exhaustion")
-        }
-
-# Function to get a random value from a specific key
-def get_random_value(key):
-    if key in answers:
-        return random.choice(answers[key])
-    else:
-        return f"Key '{key}' not found in the dictionary."
+with open(r"story_teller\files\words.json","r") as file:
+    answers = json.load(file)
     
 for word in words:
     word_rep = word.replace("[","").replace("]","")
-    story = story.replace(word,get_random_value(word_rep))
+    story = story.replace(word,random.choice(answers[word_rep]))
 
 print(story)
