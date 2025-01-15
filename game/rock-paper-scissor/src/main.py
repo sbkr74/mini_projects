07d96a4e9e1768916f4e29a11bbe990ad9763c62
sbkr74 <sbkr74@gmail.com>
2025-01-15 23:08:29 +0530

    update1.0 [modified conditions]

--- game/rock-paper-scissor/src/main.py
+++ game/rock-paper-scissor/src/main.py
@@ -15,27 +15,39 @@ while True:
     if user_input.lower() == "q":
         print("Computer: ",computer_wins)
         print("User: ",user_wins)
-        quit()
+        break
 
     com=random.choice(options)
     user=""
-    if user_input.lower()=='rock' or user_input.lower()=='r':
+    if user_input.lower() in ['rock','r']:
         user=options[0]
-    elif user_input.lower()=='paper' or user_input.lower()=='p':
+    elif user_input.lower() in ['paper','p']:
         user=options[1]
-    elif user_input.lower()=='scissor' or user_input.lower()=='s':
+    elif user_input.lower() in ['scissor','s']:
         user=options[2]
     else:
         print("Try again as suggested in Instruction")
         continue
 
-    print("com: ",com)
-    print("user: ",user)
-    if options.index(com)==options.index(user) :
-        print("Draw!!!")
-    elif options.index(com)>options.index(user) :
-        computer_wins+=1
-        print("Computer wins")
+    print("Computer picked: ",com)
+    print("You Chose: ",user)
+    
+    # Determine winner based on circular comparison
+    user_index = options.index(user)
+    com_index = options.index(com)
+
+    if user_index == com_index:
+        print("It's a tie!")
+    elif (user_index + 1) % len(options) == com_index:
+        print("Computer wins this round!")
+        computer_wins += 1
     else:
-        user_wins+=1
-        print("You won")
+        print("You win this round!")
+        user_wins += 1
+
+if computer_wins>user_wins:
+    print(f"You Lost against Computer by {computer_wins-user_wins} score.")
+elif computer_wins==user_wins:
+    print(f"Game tie at score of {user_wins}.")
+else:
+    print(f"You won against Computer by {user_wins-computer_wins} score.")
