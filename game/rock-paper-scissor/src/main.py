55fe3dfcafae8197c8ac3cc784c2143cadf85a84
sbkr74 <sbkr74@gmail.com>
2025-01-16 19:55:32 +0530

    changed code for better readability

--- game/rock-paper-scissor/src/main.py
+++ game/rock-paper-scissor/src/main.py
@@ -1,10 +1,6 @@
 import random
 
 print("\nWelcome To Rock-Paper-Scissor Game\n")
-print("Instrcutions:")
-print("for Rock Type 'Rock' or 'R'")
-print("for Paper Type 'Paper' or 'P'")
-print("for Scissor Type 'Scissor' or 'S'")
 
 user_wins = 0
 computer_wins = 0
@@ -17,33 +13,31 @@ while True:
         print("User: ",user_wins)
         break
 
-    com=random.choice(options)
-    user=""
-    if user_input.lower() in ['rock','r']:
-        user=options[0]
-    elif user_input.lower() in ['paper','p']:
-        user=options[1]
-    elif user_input.lower() in ['scissor','s']:
-        user=options[2]
-    else:
-        print("Try again as suggested in Instruction")
+    if user_input not in options:
         continue
 
+    random_num = random.randint(0,2)
+    com = options[random_num]
     print("Computer picked: ",com)
-    print("You Chose: ",user)
-    
-    # Determine winner based on circular comparison
-    user_index = options.index(user)
-    com_index = options.index(com)
 
-    if user_index == com_index:
-        print("It's a tie!")
-    elif (user_index + 1) % len(options) == com_index:
-        print("Computer wins this round!")
-        computer_wins += 1
+    if user_input == com:
+        print("It's a tie")
+        
+    elif user_input == 'rock' and com == 'scissor':
+        print("You won")
+        user_wins+=1
+
+    elif user_input == 'paper' and com == 'rock':
+        print("You won")
+        user_wins+=1
+
+    elif user_input == 'scissor' and com == 'paper':
+        print("You won")
+        user_wins+=1
+
     else:
-        print("You win this round!")
-        user_wins += 1
+        print("You lost")
+        computer_wins+=1
 
 if computer_wins>user_wins:
     print(f"You Lost against Computer by score of {computer_wins-user_wins}.")
