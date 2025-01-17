3eced6099867839bfcfee96fa0aa2da9815a6e43
sbkr74 <sbkr74@gmail.com>
2025-01-17 23:00:33 +0530

    update1.1[organised in functions]

--- password_manager/src/main.py
+++ password_manager/src/main.py
@@ -1,24 +1,23 @@
-name = input("Enter your name: ")
-access = "Denied"
-filepath = "password_manager/files/password.txt"
-while True:
-    mstr_pwd = input("Enter Master Password [0000] to Access Password Manager: ")
+def get_access():
+    access = "Denied"
+    while True:
+        mstr_pwd = input("Enter Master Password [0000] to Access Password Manager: ")
 
-    if mstr_pwd.isdigit():
-        if int(mstr_pwd) == 0000:
-            access = "Accepted"
-            break
+        if mstr_pwd.isdigit():
+            if int(mstr_pwd) == 0000:
+                access = "Accepted"
+                break
+            else:
+                break
         else:
-            break
-    else:
-        try:
-            mstr_pwd=int(mstr_pwd)
-            continue
-        except ValueError as e:
-            print("ERROR: ",e)
-            print("Accept only digits!!!\n")        
+            try:
+                mstr_pwd=int(mstr_pwd)
+                continue
+            except ValueError as e:
+                print("ERROR: ",e)
+                print("Accept only digits!!!\n")        
+    return access
 
-print(f"{name} has been autorized.")
 
 def view():
     with open(filepath,"r") as f:
@@ -31,16 +30,21 @@ def add():
     with open (filepath,"a") as f:
         f.write(name+"|"+pwd+"\n")
 
-if access == "Accepted":
-    while True:
-        choice = input("\nWould you want to view or add password. Type [view/add] or Press q or Q to Quit: ").lower()
-        if choice == 'q':
-            break
-        elif choice == "view":
-            view()
-        elif choice == "add":
-            add()
-        else:
-            print("Try again from given option [view/add/q/Q].")
+if __name__=="__main__":
+    name = input("Enter your name: ")
+    filepath = "password_manager/files/password.txt"
+    access = get_access()
+    if access == "Accepted":
+        print(f"{name} has been autorized.")
+        while True:
+            choice = input("\nWould you want to view or add password. Type [view/add] or Press q or Q to Quit: ").lower()
+            if choice == 'q':
+                break
+            elif choice == "view":
+                view()
+            elif choice == "add":
+                add()
+            else:
+                print("Try again from given option [view/add/q/Q].")
+                
             
-        
