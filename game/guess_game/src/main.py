082895e240e9d8033a1545a8bd2e264a98548b3f
sbkr74 <sbkr74@gmail.com>
2025-02-14 21:18:55 +0530

    update1.0[computer_guess() added]

--- game/guess_game/src/main.py
+++ game/guess_game/src/main.py
@@ -12,4 +12,19 @@ def guess(x):
     
     print(f"Yay! Congrats, You have guessed the number {random_guess} correctly.")
 
+def computer_guess(x):
+    high = x
+    low = 1
+    user_guess = ""
+    while user_guess != "c":
+        com_guess = random.randint(low,high)
+        user_guess = input(f"Type! If the number {com_guess} is too low[L]/ too high[H]/ correct[C]: ").lower()
+        if user_guess == "h":
+            high = com_guess-1
+        elif user_guess == "l":
+            low = com_guess+1
+    print(f"Computer has guessed the number {com_guess}")
+    
+
 guess(10)
+computer_guess(1000)
