6c5e5cb00be643f6a5734001e0555cb7ecb0fb5b
sbkr74 <sbkr74@gmail.com>
2025-03-11 22:16:32 +0530

    update1.0[return new msg]

--- word_replcement/src/main.py
+++ word_replcement/src/main.py
@@ -1,9 +1,10 @@
-def replace_word():
+def replace_word(word_to_replace,word_replacement):
     msg = "Hi Everyone. This is system generated message using python just to test word_replacement (Hi Hi Hi)"
+    new_msg = msg.replace(word_to_replace,word_replacement)
+    return new_msg
+
+if __name__ =="__main__":
     word_to_replace = input("Enter the word to replace: ")
     word_replacement = input("Enter the word you wanna change to: ")
-
-    new_msg = msg.replace(word_to_replace,word_replacement)
-    print(new_msg)
-
-replace_word()
+    msg=replace_word(word_to_replace,word_replacement)
+    print(msg)
