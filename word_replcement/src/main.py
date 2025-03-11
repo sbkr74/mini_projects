0deb66c7f581f5db6a2dc61e93024e8a28281ef7
sbkr74 <sbkr74@gmail.com>
2025-03-11 22:30:30 +0530

    update2.0[enhanced for capturing words properly]

--- word_replcement/src/main.py
+++ word_replcement/src/main.py
@@ -1,10 +1,17 @@
 def replace_word(word_to_replace,word_replacement):
     msg = "Hi Everyone. This is system generated message using python just to test word_replacement (Hi Hi Hi)"
-    new_msg = msg.replace(word_to_replace,word_replacement)
+    
+    words = msg.split(" ")
+    for i in range(len(words)):
+        if words[i] == word_to_replace:
+            words[i] = word_replacement
+        
+    new_msg = " ".join(words)
+
     return new_msg
 
 if __name__ =="__main__":
-    word_to_replace = input("Enter the word to replace: ")
-    word_replacement = input("Enter the word you wanna change to: ")
+    word_to_replace = input("Enter the word to replace: ").strip()
+    word_replacement = input("Enter the word you wanna change to: ").strip()
     msg=replace_word(word_to_replace,word_replacement)
     print(msg)
